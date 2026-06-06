ff7edaaef069eb7e9b0d3744706edaf77e33ac4e
Mudassir Khushik <mudassirkhushik@gmail.com>
2026-06-05 23:44:45 -0700

    updated cron job to delete mongoDB rooms

--- src/jobs/deleteInactiveRooms.js
+++ src/jobs/deleteInactiveRooms.js
@@ -1,21 +1,40 @@
 import cron from "node-cron"
 import Room from "../models/Room.js";
+import Message from "../models/Message.js";
 
-export const startRoomCleanupJon = ()=>{
-    cron.schedule("*/1 * * * *", async ()=>{
+export const startRoomCleanupJob = ()=>{
+    cron.schedule("*/5 * * * *", async ()=>{
         
-        const thirtyMinAgo = new Date(Date.now() - 1 * 60 * 1000);
+        try{
+        
+        const thirtyMinAgo = new Date(Date.now() - 30 * 60 * 1000);
 
-        const result  = await Room.updateMany(
-            { 
-                lastActive : {$lt : thirtyMinAgo},
-                isDeleted : false  
-            },
-            {
-                isDeleted : true
-            }
-        );
-        console.log( `Marked ${result.modifiedCount} rooms deleted`);
+        const inactiveRooms = await Room.find({
+            lastActive : {$lt : thirtyMinAgo}
+        });
+        console.log("Incative rooms : ",inactiveRooms)
+        
+        if(inactiveRooms.length === 0){
+            console.log("No inactive rooms found");
+            return;
+        }
+
+        for(const room of inactiveRooms){
+            await Message.deleteMany({
+                roomCode : room.roomCode
+            });
+            
+            await Room.deleteOne({
+                _id : room._id
+            });
+            
+            console.log(`Deleted Rooms : ${room.name} (${room.roomCode})`);
+        }
+        
+    }catch(error){
+        console.log("Room cleanup error : " , error.message);
+        
+    }
     });
 
 };
